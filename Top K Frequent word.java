c7f2b16a740295befbb8ea96a3fafe425dbc31e3
pabloalonso-20 <62111010+pabloalonso-20@users.noreply.github.com>
2020-10-07 18:54:45 -0500

    Update Top K Frequent word.java

--- Top K Frequent word.java
+++ Top K Frequent word.java
@@ -1,6 +1,8 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
     
+        //creates a new hash map with string type keys and Integer type values
+        //the strings will be the words that are inputted and the values will be the count for each word
         HashMap<String, Integer> myMap = new HashMap();
         
         //counts how many times that word is mentioned in input
@@ -13,7 +15,7 @@ class Solution {
         
         //collections sort arranges them 
         //compare to checks if they are equal
-        
+        //found this bit online (unsure how the -> works exactly though) Pointer?
         Collections.sort(possible, (w1, w2) -> myMap.get(w1).equals(myMap.get(w2)) ?
                 w1.compareTo(w2) : myMap.get(w2) - myMap.get(w1));
         
